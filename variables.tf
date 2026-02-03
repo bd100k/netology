6c3d7002778a8118284a7eb93dc6496a5516fc3f
Yury <58076883+bd100k@users.noreply.github.com>
2026-02-03 19:42:15 +0400

    Created

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "flow" {
+  type    = string
+  default = "03-02"
+}
+
+variable "cloud_id" {
+  type    = string
+  default = "b1g1hu5dn45ohv4r8olk"
+}
+
+variable "folder_id" {
+  type    = string
+  default = "b1gbpi3k9304s9tseshg"
+}
